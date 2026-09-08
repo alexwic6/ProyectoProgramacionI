f9a7b35274540d11096ee32cad195f4f1e214e7a
alexwic6 <alexwic6@gmail.com>
2026-09-08 19:56:52 -0300

    Inicialización de matriz y declaración de tuplas

---
+++ techcontrol/matriz.py
@@ -0,0 +1,7 @@
+filas = 3
+columnas = 4
+
+matriz = [[0 for j in range(columnas)] for i in range(filas)]
+
+TIPOS = ("Computadora", "Monitor", "Celular")
+ESTADOS = ("Disponible", "Asignado", "En reparación", "Fuera de servicio")
