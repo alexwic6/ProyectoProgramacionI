e8caecbe481d265e4556aaa93822c58393295fb9
alexwic6 <alexwic6@gmail.com>
2026-09-10 13:32:34 -0300

    Función registrar_empleado

--- techcontrol/empleados.py
+++ techcontrol/empleados.py
@@ -1,2 +1,21 @@
 legajos_empleados = []
 nombres_empleados = []
+
+
+#   FUNCIÓN REGISTRAR EMPLEADO
+
+def registrar_empleado(legajo, nombre):
+    if legajo == "" or legajo == None:
+        print("El legajo no puede estar vacío.")
+        return
+    
+    if legajo in legajos_empleados:
+        print(f"El legajo '{legajo}' ya existe.")
+        return
+    
+    if nombre == "" or nombre == None:
+        print("El nombre no puede estar vacío")
+        return
+
+    legajos_empleados.append(legajo)
+    nombres_empleados.append(nombre)
